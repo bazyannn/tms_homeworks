7601c0650666d5bb2c16cbe751aaebe61e4b9184
Viachaslau Sakovich <160413593+bazyannn@users.noreply.github.com>
2024-09-26 13:11:38 +0300

    Revert "Revert "Rest + Swagger""
    This reverts commit 637dd4e7372742359a53c1cd70aec155585c931e.

--- Rest_43_1/src/main/java/com/example/rest_43_1/web/RentController.java
+++ Rest_43_1/src/main/java/com/example/rest_43_1/web/RentController.java
@@ -1,14 +1,10 @@
 package com.example.rest_43_1.web;
 
 import com.example.rest_43_1.dto.BicycleDto;
-import com.example.rest_43_1.dto.ErrorDto;
 import com.example.rest_43_1.dto.PaginationDto;
 import com.example.rest_43_1.dto.SearchDto;
 import com.example.rest_43_1.service.RentService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,32 +23,6 @@ public class RentController {
         return service.findAll(dto);
     }
 
-    @Operation(tags = "rent",
-            summary = "Find bicycle by ID",
-            description = "find bicycle from DB by ID",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Bicycle was found",
-                            content = @Content(
-                                    mediaType = "application/application.json",
-                                    schema = @Schema(
-                                            implementation = BicycleDto.class
-                                    )
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "Bicycle not found",
-                            content = @Content(
-                                    mediaType = "application/application.json",
-                                    schema = @Schema(
-                                            implementation = ErrorDto.class
-                                    )
-                            )
-                    )
-            }
-    )
     @GetMapping("/{id}")
     public BicycleDto findById(@PathVariable(name = "id") UUID id) {
         return service.findById(id);
